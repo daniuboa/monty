415d289346696653e010b43a024a5280d4c5e9a9
Daniel Uboa <uboadaniel@gmail.com>
2022-07-13 03:17:05 -0700

    Edit _push.c

--- _push.c
+++ _push.c
@@ -9,26 +9,13 @@
  */
 void _push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *element = malloc(sizeof(stack_t));
-	char *opcode;
-	int num;
+	stack_t *new = NULL;
+	(void)line_number;
 
-	if (!element)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-	opcode = strtok(NULL, "\n\t\r ");
-	if (opcode == NULL || stack == NULL)
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	num = interpreter(opcode, line_number);
-	element->n = num;
-	element->prev = NULL;
-	element->next = *stack;
-	if (element->next != NULL)
-		(element->next)->prev = element;
-	*stack = element;
+	new = new_Node(value);
+
+	new->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new;
+	*stack = new;
 }
