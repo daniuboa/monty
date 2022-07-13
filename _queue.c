fdd704dbfdb8a8b86a6ae6aac543e0d758b790e6
Daniel Uboa <uboadaniel@gmail.com>
2022-07-13 03:29:04 -0700

    Edit _queue.c

--- _queue.c
+++ _queue.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * _queue - function that sets the format of the data to a queue (FIFO).
+ * _queue - onverts stacks to a queue.
  * @stack: double pointer to the head of stack.
  * @line_number: script line number
  *
@@ -9,7 +9,6 @@
  */
 void _queue(stack_t **stack, unsigned int line_number)
 {
-	(void)(stack);
-	(void)(line_number);
-	var.queue = QUEUE;
+	(*stack)->n = QUEUE;
+	(void)line_number;
 }
