e033cbbf90e6e556dd59c79d20685211660936c8
Daniel Uboa <uboadaniel@gmail.com>
2022-07-13 03:03:32 -0700

    Edit _push.c

--- _push.c
+++ _push.c
@@ -5,13 +5,6 @@
  * @stack: double pointer to the head of stack.
  * @line_number: script line number.
  *
- * Usage: push <int> where <int> is an integer.
- * if <int> is not an integer or if there is no argument given to push,
- * print the error message L<line_number>: usage: push integer, followed,
- * by a new line, and exit with the status EXIT_FAILURE where is the line,
- * number in the file.
- * You won’t have to deal with overflows. Use the atoi function.
- *
  * Return: No return.
  */
 void _push(stack_t **stack, unsigned int line_number)
